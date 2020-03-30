be920f4f4e52aed4faadf04b16e1b13af1674b8c
Tran Tuan <tuanntgch16490@fpt.edu.vn>
2020-03-30 13:50:14 +0700

    Feat(Backend): Modified function crd Major

--- app/Http/Controllers/MajorController.php
+++ app/Http/Controllers/MajorController.php
@@ -2,34 +2,84 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Major;
 use Illuminate\Http\Request;
+use App\Models\Major;
+use App\Http\Controllers\Controller;
 use App\Repositories\Major\MajorRepositoryInterface;
 
 class MajorController extends Controller
 {
     protected $majorRepository;
-    public function __construct(
-        MajorRepositoryInterface $majorRepository
-    ) {
+
+    public function __construct(MajorRepositoryInterface $majorRepository)
+    {
         $this->majorRepository = $majorRepository;
     }
 
     public function index()
     {
-        //
+        try {
+            $majors = $this->majorRepository->getAll();
+
+            return response()->json([
+                'results' => $majors,
+                'success' => true,
+                'message' => 'Show majors successfully!',
+            ]);
+        } catch (\Exception $ex) {
+            report($ex);
+
+            return response()->json([
+                'results' => null,
+                'success' => false,
+                'message' => $ex,
+            ]);
+        }
     }
 
     public function store(Request $request)
     {
-        //
+        try {
+            $data = $request->all();
+            $major = $this->majorRepository->create($data);
+
+            return response()->json([
+                'result' => $major,
+                'success' => true,
+                'message' => 'Created major successfully',
+            ]);
+        } catch (\Exception $ex) {
+            report($ex);
+
+            return response()->json([
+                'results' => null,
+                'success' => false,
+                'message' => $ex,
+            ]);
+        }
     }
 
-    public function edit($id)
+    public function show($id)
     {
-        //
+        try {
+            $major = Major::find($id);
+
+            return response()->json([
+                'result' => $major,
+                'success' => true,
+                'message' => 'Successfully',
+            ]);
+        } catch (\Exception $ex) {
+            report($ex);
+
+            return response()->json([
+                'results' => null,
+                'success' => false,
+                'message' => $ex,
+            ]);
+        }
     }
-    
+   
     public function update(Request $request, $id)
     {
         try {
@@ -53,6 +103,22 @@ class MajorController extends Controller
 
     public function destroy($id)
     {
-        //
+        try {
+            $major = $this->majorRepository->delete($id);
+
+            return response()->json([
+                'results' => $major,
+                'success' => true,
+                'message' => 'Delete major successfully!',
+            ]);
+        } catch (\Exception $ex) {
+            report($ex);
+
+            return response()->json([
+                'results' => null,
+                'success' => false,
+                'message' => $ex,
+            ]);
+        }
     }
 }
