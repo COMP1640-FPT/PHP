2a3c5e8897bc80936d822d4f877390f905d64090
hainh-0963 <nguyen.huu.hai@sun-asterisk.com>
2020-04-19 21:39:34 +0700

    Fix file seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -50,7 +50,7 @@ class UserTableSeeder extends Seeder
         ]);
 
         DB::table('users')->insert([
-            "name" => "User Student",
+            "name" => "Nguyen Huu Hai",
             "code" => "GCH10001",
             "role" => "student",
             "country" => "Vietnam",
@@ -58,7 +58,77 @@ class UserTableSeeder extends Seeder
             "gender" => "1",
             "phone" => "0123456789",
             "birthday" => "2020-03-24",
-            "email" => "student@etutor.com",
+            "email" => "hainhgch16440@fpt.edu.vn",
+            "updated_at" => Carbon::now(),
+            "created_at" => Carbon::now(),
+        ]);
+
+        DB::table('users')->insert([
+            "name" => "To Hai Nam",
+            "code" => "GCH10002",
+            "role" => "student",
+            "country" => "Vietnam",
+            "address" => "PC",
+            "gender" => "1",
+            "phone" => "0123456789",
+            "birthday" => "2020-03-24",
+            "email" => "namthgch16500@fpt.edu.vn",
+            "updated_at" => Carbon::now(),
+            "created_at" => Carbon::now(),
+        ]);
+
+        DB::table('users')->insert([
+            "name" => "Dang Xuan Khanh",
+            "code" => "GCH10003",
+            "role" => "student",
+            "country" => "Vietnam",
+            "address" => "PC",
+            "gender" => "1",
+            "phone" => "0123456789",
+            "birthday" => "2020-03-24",
+            "email" => "khanhdxgch16549@fpt.edu.vn",
+            "updated_at" => Carbon::now(),
+            "created_at" => Carbon::now(),
+        ]);
+
+        DB::table('users')->insert([
+            "name" => "Nguyen Tran Tuan",
+            "code" => "GCH10004",
+            "role" => "student",
+            "country" => "Vietnam",
+            "address" => "PC",
+            "gender" => "1",
+            "phone" => "0123456789",
+            "birthday" => "2020-03-24",
+            "email" => "tuanntgch16490@fpt.edu.vn",
+            "updated_at" => Carbon::now(),
+            "created_at" => Carbon::now(),
+        ]);
+
+        DB::table('users')->insert([
+            "name" => "Hoang The Viet",
+            "code" => "GCH10005",
+            "role" => "student",
+            "country" => "Vietnam",
+            "address" => "PC",
+            "gender" => "1",
+            "phone" => "0123456789",
+            "birthday" => "2020-03-24",
+            "email" => "viethtgch16100@fpt.edu.vn",
+            "updated_at" => Carbon::now(),
+            "created_at" => Carbon::now(),
+        ]);
+
+        DB::table('users')->insert([
+            "name" => "Nguyễn Thúy Hà",
+            "code" => "GCH10006",
+            "role" => "student",
+            "country" => "Vietnam",
+            "address" => "PC",
+            "gender" => "0",
+            "phone" => "0123456789",
+            "birthday" => "2020-03-24",
+            "email" => "hantgch16343@fpt.edu.vn",
             "updated_at" => Carbon::now(),
             "created_at" => Carbon::now(),
         ]);
